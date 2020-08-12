9aa0ebc1e57c179fdbbdbb38d89a79e22e00552a
OToNaShiAKi <1362446747@qq.com>
2020-08-12 23:57:57 +0800

    rank

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app id="app">
     <div>
-      <v-img class="title" :src="title" width="275"/>
-      <v-img class="leaf" :src="free" width="186"/>
+      <v-img class="title" :src="title" width="275" />
+      <v-img class="leaf" :src="free" width="186" />
     </div>
     <v-list class="wins" dense subheader>
       <v-subheader>获奖名单</v-subheader>
@@ -14,16 +14,17 @@
         <v-list-tile-title>{{v.nick}}</v-list-tile-title>
         <v-list-tile-action>{{v.score}}</v-list-tile-action>
         <v-list-tile-avatar>
-          <v-img :src="zongzi"/>
+          <v-img :src="zongzi" />
           &times;{{6 - i}}
         </v-list-tile-avatar>
       </v-list-tile>
-      <v-divider/>
+      <v-divider />
       <v-subheader>细则</v-subheader>
       <v-list-tile v-for="(v, i) in know" :key="i">
         <v-list-tile-content>{{v}}</v-list-tile-content>
       </v-list-tile>
     </v-list>
+    <start />
   </v-app>
 </template>
 
@@ -31,9 +32,13 @@
 import title from "@/assets/title.png";
 import free from "@/assets/free.png";
 import zongzi from "@/assets/zongzi.png";
+import start from "./components/start";
 
 export default {
   name: "app",
+  components: {
+    start,
+  },
   data: () => ({
     title,
     free,
@@ -44,7 +49,7 @@ export default {
       "orange lighten-2",
       "primary",
       "accent",
-      "secondary"
+      "secondary",
     ],
     text: [
       "yellow--text text--lighten-2",
@@ -52,7 +57,7 @@ export default {
       "orange--text --text--lighten-2",
       "primary--text",
       "error--text",
-      "secondary--text"
+      "secondary--text",
     ],
     wins: [
       { name: "aaa", nick: "bbb", score: 0 },
@@ -60,16 +65,16 @@ export default {
       { name: "aaa", nick: "bbb", score: 0 },
       { name: "aaa", nick: "bbb", score: 0 },
       { name: "aaa", nick: "bbb", score: 0 },
-      { name: "aaa", nick: "bbb", score: 0 }
+      { name: "aaa", nick: "bbb", score: 0 },
     ],
     know: [
       "获奖短信将于6月10日11:00发送完毕，请在6月10日15:30-20:30于紫崧10栋领取。",
       "领取时请凭获奖短信和华科学生卡领取。",
       "若为他人代领，请转发短信并转交学生卡。",
       "该名单已剔除未填写信息用户。",
-      "为省去忘记密码时找回的繁琐，允许不同账户绑定同一姓名、手机、电话，颁奖时已剔除相同用户。"
-    ]
-  })
+      "为省去忘记密码时找回的繁琐，允许不同账户绑定同一姓名、手机、电话，颁奖时已剔除相同用户。",
+    ],
+  }),
 };
 </script>
 
